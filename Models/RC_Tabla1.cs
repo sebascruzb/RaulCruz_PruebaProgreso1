7a177bf3d86261edfada479e6f3a4ab174ecb70f
Raul Cruz <sebascruzb03@hotmail.com>
2024-04-24 11:15:07 -0500

    Creacion Tabla

---
+++ Models/RC_Tabla1.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RaulCruz_PruebaProgreso1.Models
+{
+    public class RC_Tabla1
+    {
+        public int RCId { get; set; }
+
+        [Required]
+        public string? RCName { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal RCValor { get; set; }
+        public DateTime RCFechaInicio { get; set; }
+        public bool RCBoleano { get; set; }
+    }
+}
